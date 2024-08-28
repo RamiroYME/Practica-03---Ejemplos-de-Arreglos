d78f29bb9cb9be512add6e203bc348260080603f
maris <maris@LAPTOPHP>
2024-08-27 18:26:15 -0600

    Agregar archivos de proyecto.

---
+++ Practica 03 - Ejemplos de Arreglos/Practica 03 - Ejemplos de Arreglos.cpp
@@ -0,0 +1,4 @@
+int números[6] = (10, 20, 30, 40, 50, 60);
+int n[] = (3, 4, 5)//Declara un array de 3 elementos
+char c[] = ('L','u','i','s');//Declara un array de 4 elementos
+char s[] = "Mona Lisa";
---
+++ Practica 03 - Ejemplos de Arreglos/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+numeros = [10, 20, 30, 40, 50, 60]
+n = [3, 4, 5]
+nombre = ['L', 'u', 'i', 's']
+s = "Mona Lisa"
